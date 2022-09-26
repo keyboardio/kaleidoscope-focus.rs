ae8b80ed416969972f9f5517ae373548339df69a
Gergely Nagy <algernon@keyboard.io>
2022-09-27 00:42:10 +0200

    Add a new tool: focus
    Unlike the `focus-send` tool, this is not CLI-compatible with the shell script
    version of `focus-send` in Kaleidoscope, but is a superset of it, which can do
    other things besides sending commands. Like listing available devices.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

---
+++ src/bin/focus.rs
@@ -0,0 +1,102 @@
+// focus -- focus interaction tool
+// Copyright (C) 2022  Keyboard.io, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use clap::{Args, Parser, Subcommand};
+use indicatif::ProgressBar;
+use kaleidoscope::Focus;
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    #[arg(short, long, env, hide_env = true, value_name = "PATH")]
+    /// The device to connect to
+    device: Option<String>,
+
+    #[arg(short, long, default_value = "false")]
+    /// Operate quietly
+    quiet: bool,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+#[command(version, about)]
+enum Commands {
+    /// List available, focus-capable devices
+    ListDevices,
+    /// Send a request to the keyboard, and display the reply
+    Send(Send),
+}
+
+#[derive(Args)]
+struct Send {
+    /// The command to send
+    command: String,
+    /// Optional arguments for <COMMAND>
+    args: Vec<String>,
+}
+
+fn list_devices() {
+    kaleidoscope::find_devices()
+        .expect("No supported devices found")
+        .iter()
+        .for_each(|device| {
+            println!("{}", device);
+        });
+}
+
+fn send(device: Option<String>, opts: &Send) {
+    let device_path = match device {
+        Some(d) => d,
+        None => kaleidoscope::find_devices().expect("No supported device found")[0].clone(),
+    };
+
+    let mut focus = Focus::create(&device_path).open().unwrap_or_else(|e| {
+        eprintln!("Failed to open \"{}\". Error: {}", &device_path, e);
+        ::std::process::exit(1);
+    });
+
+    let pb = if !opts.args.is_empty() {
+        ProgressBar::new(100)
+    } else {
+        ProgressBar::hidden()
+    };
+    focus.flush().unwrap();
+    focus
+        .request_with_progress(
+            opts.command.to_string(),
+            Some(opts.args.clone()),
+            |l| {
+                pb.set_length(l.try_into().unwrap());
+            },
+            |c| {
+                pb.inc(c.try_into().unwrap());
+            },
+        )
+        .expect("failed to send the request to the keyboard");
+    pb.finish_and_clear();
+    let reply = focus.read_reply().expect("failed to read the reply");
+    println!("{}", reply);
+}
+
+fn main() {
+    let opts = Cli::parse();
+
+    match &opts.command {
+        Commands::ListDevices => list_devices(),
+        Commands::Send(s) => send(opts.device, s),
+    }
+}
