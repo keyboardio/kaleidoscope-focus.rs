7cff0a3ff1eb13037de622bc765bac4b766302fa
Gergely Nagy <algernon@keyboard.io>
2022-10-16 14:24:01 +0200

    bin/focus: Implement backup & restore commands
    Requires keyboardio/Kaleidoscope#1280.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/bin/focus.rs
+++ src/bin/focus.rs
@@ -14,8 +14,11 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use clap::{Args, Parser, Subcommand};
-use indicatif::ProgressBar;
+use indicatif::{ProgressBar, ProgressStyle};
 use kaleidoscope_focus::Focus;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::io;
 
 #[derive(Parser)]
 #[command(version, about)]
@@ -39,6 +42,10 @@ enum Commands {
     ListPorts,
     /// Send a request to the keyboard, and display the reply
     Send(Send),
+    /// Create a backup of the keyboards configuration
+    Backup,
+    /// Restore the keyboards configuration from backup
+    Restore(Restore),
 }
 
 #[derive(Args)]
@@ -53,12 +60,22 @@ struct Send {
     args: Vec<String>,
 }
 
+#[derive(Args)]
+struct Restore {
+    #[arg(short, long, default_value = "32")]
+    /// Set the size of the buffer used to send data. Setting it to 0 writes
+    /// everything all at once
+    chunk_size: usize,
+}
+
 fn main() {
     let opts = Cli::parse();
 
     match &opts.command {
         Commands::ListPorts => list_ports(),
         Commands::Send(s) => send(s, &opts),
+        Commands::Backup => backup(&opts),
+        Commands::Restore(r) => restore(r, &opts),
     }
 }
 
@@ -99,4 +116,87 @@ fn send(opts: &Send, main_opts: &Cli) {
     if !reply.is_empty() {
         println!("{}", reply);
     }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Backup {
+    commands: HashMap<String, String>,
+}
+
+fn backup(main_opts: &Cli) {
+    let device_path = match &main_opts.device {
+        Some(d) => d.to_string(),
+        None => kaleidoscope_focus::find_devices().expect("No supported device found")[0].clone(),
+    };
+
+    let mut focus = Focus::create(&device_path).open().unwrap_or_else(|e| {
+        eprintln!("Failed to open \"{}\". Error: {}", &device_path, e);
+        ::std::process::exit(1);
+    });
+
+    focus.flush().unwrap();
+    focus
+        .request("backup".to_string(), None, None)
+        .expect("Failed to request backup eligible commands");
+    let reply = focus
+        .read_reply()
+        .expect("failed to read the list of backup eligible commands");
+
+    let backup_commands: Vec<&str> = reply.lines().collect();
+    if backup_commands.is_empty() {
+        eprintln!("The `backup` command appears to be unsupported by the firmware.");
+        ::std::process::exit(1);
+    }
+
+    let mut backup = Backup {
+        commands: HashMap::new(),
+    };
+    let pb = if main_opts.quiet {
+        ProgressBar::hidden()
+    } else {
+        ProgressBar::new(backup_commands.len().try_into().unwrap())
+    };
+    backup_commands.iter().for_each(|cmd| {
+        focus
+            .request(cmd.to_string(), None, None)
+            .expect("Failed to send command");
+        let reply = focus.read_reply().expect("Failed to read a reply");
+        backup.commands.insert(cmd.to_string(), reply);
+        pb.inc(1);
+    });
+    pb.finish_and_clear();
+    println!("{}", serde_json::to_string(&backup).unwrap());
+}
+
+fn restore(opts: &Restore, main_opts: &Cli) {
+    let backup: Backup = serde_json::from_reader(io::stdin()).expect("Unable to parse the backup");
+
+    let device_path = match &main_opts.device {
+        Some(d) => d.to_string(),
+        None => kaleidoscope_focus::find_devices().expect("No supported device found")[0].clone(),
+    };
+
+    let mut focus = Focus::create(&device_path)
+        .chunk_size(opts.chunk_size)
+        .open()
+        .unwrap_or_else(|e| {
+            eprintln!("Failed to open \"{}\". Error: {}", &device_path, e);
+            ::std::process::exit(1);
+        });
+
+    let pb = if main_opts.quiet {
+        ProgressBar::hidden()
+    } else {
+        ProgressBar::new(backup.commands.len().try_into().unwrap())
+    };
+    pb.set_style(ProgressStyle::with_template("{spinner} {pos} / {len} ({msg}) ").unwrap());
+    backup.commands.iter().for_each(|(k, v)| {
+        pb.set_message(k.clone());
+        focus
+            .request(k.to_string(), Some(vec![v.to_string()]), None)
+            .expect("Restoration failed");
+        focus.read_reply().expect("Restoration failed");
+        pb.inc(1);
+    });
+    pb.finish_and_clear();
 }
