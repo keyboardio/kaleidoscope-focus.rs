a398b5d51dbcebc4f569deb60eab5dc158e5eab9
Gergely Nagy <algernon@keyboard.io>
2022-10-18 17:39:26 +0200

    Document most of the library code
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/lib.rs
+++ src/lib.rs
@@ -13,62 +13,52 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#![warn(missing_docs)]
+
+//! **Talking to [`Kaleidoscope`] powered devices with Rust**
+//!
+//! This library is a very thin layer on top of `serialport`, implementing a
+//! handful of convenience functions to make it easy to communicate with devices
+//! speaking Kaleidoscope's [`Focus`] protocol.
+//!
+//! Start at [`struct.Focus`] to discover what the crate provides.
+//!
+//! [`struct.Focus`]: ./struct.Focus.html
+//! [`Kaleidoscope`]: https://github.com/keyboardio/Kaleidoscope
+//! [`Focus`]: https://kaleidoscope.readthedocs.io/en/latest/plugins/Kaleidoscope-FocusSerial.html
+
 use serialport::SerialPort;
 use std::io::{self, Write};
 use std::thread;
 use std::time::Duration;
 
-pub trait ProgressReport {
-    fn set_length(&self, length: usize);
-    fn progress(&self, delta: usize);
-}
-
-impl ProgressReport for indicatif::ProgressBar {
-    fn set_length(&self, length: usize) {
-        self.set_length(length.try_into().unwrap());
-    }
-    fn progress(&self, delta: usize) {
-        self.inc(delta.try_into().unwrap());
-    }
-}
-
+/// The representation of a connection to a keyboard, used for all communication.
+///
+/// Constructed using a builder pattern, using [`Focus::create`].
 pub struct Focus {
     port: Box<dyn SerialPort>,
     chunk_size: usize,
     interval: u64,
 }
 
-pub struct FocusBuilder<'a> {
-    device: &'a str,
-    chunk_size: usize,
-    interval: u64,
-}
-
-impl FocusBuilder<'_> {
-    pub fn open(&self) -> Result<Focus, serialport::Error> {
-        let port = serialport::new(self.device, 115200)
-            .timeout(Duration::from_millis(self.interval))
-            .open()?;
-
-        Ok(Focus {
-            port,
-            chunk_size: self.chunk_size,
-            interval: self.interval,
-        })
-    }
-
-    pub fn chunk_size(&mut self, chunk_size: usize) -> &Self {
-        self.chunk_size = chunk_size;
-        self
-    }
-
-    pub fn interval(&mut self, interval: u64) -> &Self {
-        self.interval = interval;
-        self
-    }
-}
-
 impl Focus {
+    /// Create a new connection using a Builder pattern.
+    ///
+    /// A `device` to open must be specified. What the `device` is, is platform
+    /// dependent, see [`serialport::new`] for more information.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use kaleidoscope_focus::Focus;
+    /// # fn main() -> Result<(), std::io::Error> {
+    /// let mut conn = Focus::create("/dev/ttyACM0")
+    ///     .chunk_size(32)
+    ///     .interval(50)
+    ///     .open()?;
+    /// #   Ok(())
+    /// # }
+    /// ```
     pub fn create(device: &str) -> FocusBuilder {
         FocusBuilder {
             device,
@@ -77,12 +67,37 @@ impl Focus {
         }
     }
 
-    pub fn flush(&mut self) -> Result<(), std::io::Error> {
-        self.request(String::from(" "), None, None)?;
-        self.read_reply()?;
-        Ok(())
-    }
-
+    /// Send a request to the keyboard.
+    ///
+    /// Sends a `command` request to the keyboard, with optional `args`, and
+    /// with optional progress reporting via `progress_report`. Does not wait or
+    /// read for a reply.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use kaleidoscope_focus::Focus;
+    /// # fn main() -> Result<(), std::io::Error> {
+    /// let mut conn = Focus::create("/dev/ttyACM0").open()?;
+    /// let request = conn.request("help".to_string(), None, None);
+    /// assert!(request.is_ok());
+    /// #   Ok(())
+    /// # }
+    /// ```
+    ///
+    /// ```rust
+    /// # use kaleidoscope_focus::Focus;
+    /// # use indicatif::ProgressBar;
+    /// # fn main() -> Result<(), std::io::Error> {
+    /// let mut conn = Focus::create("/dev/ttyACM0").open()?;
+    /// let progress = ProgressBar::new(0);
+    /// let request = conn.request("keymap.onlyCustom".to_string(),
+    ///                            Some(vec!["1".to_string()]),
+    ///                            Some(&progress));
+    /// assert!(request.is_ok());
+    /// #   Ok(())
+    /// # }
+    /// ```
     pub fn request(
         &mut self,
         command: String,
@@ -114,6 +129,22 @@ impl Focus {
         Ok(())
     }
 
+    /// Reads a reply from the keyboard.
+    ///
+    /// Once a command has been sent, the keyboard will reply something. May
+    /// return an empty string if the command is unknown, or if it had no
+    /// output.
+    ///
+    /// ```rust
+    /// # use kaleidoscope_focus::Focus;
+    /// # fn main() -> Result<(), std::io::Error> {
+    /// let mut conn = Focus::create("/dev/ttyACM0").open()?;
+    /// conn.request("settings.version".to_string(), None, None);
+    /// let reply = conn.read_reply()?;
+    /// assert_eq!(reply, "1 ");
+    /// #   Ok(())
+    /// # }
+    /// ```
     pub fn read_reply(&mut self) -> Result<String, std::io::Error> {
         let mut buffer: Vec<u8> = vec![0; 1024];
         let mut reply = vec![];
@@ -145,6 +176,34 @@ impl Focus {
             .join("\n"))
     }
 
+    /// Flush any pending data.
+    ///
+    /// Sends an empty command, and then waits until the keyboard stops sending
+    /// data. The intended use is to clear any pending I/O operations in flight.
+    ///
+    /// ```rust
+    /// # use kaleidoscope_focus::Focus;
+    /// # fn main() -> Result<(), std::io::Error> {
+    /// let mut conn = Focus::create("/dev/ttyACM0").open()?;
+    ///
+    /// /// Send a request whose output we're not interested in.
+    /// conn.request("help".to_string(), None, None)?;
+    /// /// Flush it!
+    /// conn.flush()?;
+    ///
+    /// /// ...and then send the request we want the output of.
+    /// conn.request("settings.version".to_string(), None, None)?;
+    /// let reply = conn.read_reply()?;
+    /// assert_eq!(reply, "1 ");
+    /// #   Ok(())
+    /// # }
+    /// ```
+    pub fn flush(&mut self) -> Result<(), std::io::Error> {
+        self.request(String::from(" "), None, None)?;
+        self.read_reply()?;
+        Ok(())
+    }
+
     fn wait_for_data(&mut self) -> Result<(), std::io::Error> {
         while self.port.bytes_to_read()? == 0 {
             thread::sleep(Duration::from_millis(self.interval));
@@ -153,6 +212,68 @@ impl Focus {
     }
 }
 
+/// Provides a builder pattern for [`Focus`].
+///
+/// Use [`Focus::create`] to start building.
+pub struct FocusBuilder<'a> {
+    device: &'a str,
+    chunk_size: usize,
+    interval: u64,
+}
+
+impl FocusBuilder<'_> {
+    /// Set the chunk size to use for writes.
+    ///
+    /// The library uses chunked writes by default, to work around old firmware
+    /// bugs, and operating system quirks at times. Use this method to set the
+    /// chunk size to your desired value.
+    ///
+    /// Setting the size to 0 disables chunking.
+    ///
+    /// See [`Focus::create`] for an example.
+    pub fn chunk_size(mut self, chunk_size: usize) -> Self {
+        self.chunk_size = chunk_size;
+        self
+    }
+
+    /// Set the interval between chunks.
+    ///
+    /// See [`Focus::create`] for an example.
+    pub fn interval(mut self, interval: u64) -> Self {
+        self.interval = interval;
+        self
+    }
+
+    /// Open a connection to the keyboard.
+    ///
+    /// Stops building the configuration for the [`Focus`] struct, and opens a
+    /// connection to the keyboard.
+    ///
+    /// See [`Focus::create`] for an example.
+    pub fn open(&self) -> Result<Focus, serialport::Error> {
+        let port = serialport::new(self.device, 115200)
+            .timeout(Duration::from_millis(self.interval))
+            .open()?;
+
+        Ok(Focus {
+            port,
+            chunk_size: self.chunk_size,
+            interval: self.interval,
+        })
+    }
+}
+
+/// Find supported devices, and return the paths to their ports.
+///
+/// Iterates over available USB serial ports, and keeps only those that belong
+/// to a supported keyboard. The crate only recognises Keyboardio devices as
+/// supported keyboards.
+///
+/// ```rust
+/// # use kaleidoscope_focus::find_devices;
+/// let devices = find_devices().unwrap();
+/// assert!(devices.len() > 0);
+/// ```
 pub fn find_devices() -> Option<Vec<String>> {
     #[derive(PartialEq)]
     struct DeviceDescriptor {
@@ -202,4 +323,26 @@ pub fn find_devices() -> Option<Vec<String>> {
     }
 
     Some(devices)
+}
+
+/// A trait used to implement progress reporting.
+///
+/// See [`Focus.request`] for an example.
+///
+/// The crate provides an implementation of the trait for
+/// [`indicatif::ProgressBar`].
+pub trait ProgressReport {
+    #[allow(missing_docs)]
+    fn set_length(&self, length: usize);
+    #[allow(missing_docs)]
+    fn progress(&self, delta: usize);
+}
+
+impl ProgressReport for indicatif::ProgressBar {
+    fn set_length(&self, length: usize) {
+        self.set_length(length.try_into().unwrap());
+    }
+    fn progress(&self, delta: usize) {
+        self.inc(delta.try_into().unwrap());
+    }
 }
